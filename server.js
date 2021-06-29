b812a3f99b2a09ef14709afdf25ee617f93e6c1c
Nasser BOUKEHIL <boukehil.nasreddine@gmail.com>
2021-06-29 19:57:18 +0200

    added express and fixed fetching data

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
 require('dotenv').config()
+
+const express = require('express')
+
+const app = express()
+
+const { google } = require('googleapis')
+const scopes = ['https://www.googleapis.com/auth/analytics', 'https://www.googleapis.com/auth/analytics.edit']
+const jwt = new google.auth.JWT(process.env.CLIENT_EMAIL, null, process.env.PRIVATE_KEY, scopes)
+
+async function getData(){
+    const response = await jwt.authorize()
+    const result = await google.analytics('v3').management.webproperties.list({
+        'auth': jwt,
+        'accountId': process.env.ACCOUNT_ID
+    })
+
+    console.log(result.data.totalResults)
+}
+
+getData()
+
+
+app.listen(3000, () => console.log('Server Ready'))
