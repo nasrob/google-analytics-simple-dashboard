50a325c83abb7fbb9abd6566cb1ffb33b114fc3a
Nasser BOUKEHIL <boukehil.nasreddine@gmail.com>
2021-06-30 20:57:03 +0200

    added getDailyData

--- server.js
+++ server.js
@@ -8,14 +8,46 @@ const { google } = require('googleapis')
 const scopes = ['https://www.googleapis.com/auth/analytics', 'https://www.googleapis.com/auth/analytics.edit']
 const jwt = new google.auth.JWT(process.env.CLIENT_EMAIL, null, process.env.PRIVATE_KEY, scopes)
 
-async function getData(){
+async function getPropertiesList(){
     const response = await jwt.authorize()
     const result = await google.analytics('v3').management.webproperties.list({
         'auth': jwt,
         'accountId': process.env.ACCOUNT_ID
     })
 
-    console.log(result.data.totalResults)
+    // get properties names if defaultProfileId exists
+    return result.data.items.map(item => {
+        return item.defaultProfileId ? { name: item.name, id: item.defaultProfileId } : false
+    })
+}
+
+async function getDailyData(viewId, startDate, endDate) {
+    const analyticsReporting = google.analyticsreporting({
+        version: 'v4',
+        auth: jwt
+    })
+
+    const res = await analyticsReporting.reports.batchGet({
+        requestBody: {
+            reportRequests: [{
+                viewId: viewId,
+                dateRanges: [{
+                    startDate: startDate,
+                    endDate: endDate
+                }],
+                metrics: [{
+                    expression: 'ga:sessions'
+                }]
+            }]
+        }
+    })
+
+    return res.data.reports[0].data.totals[0].values[0]
+}
+
+async function getData() {
+    console.log(await getPropertiesList())
+    console.log(await getDailyData('222499599', 'today', 'today'))
 }
 
 getData()
